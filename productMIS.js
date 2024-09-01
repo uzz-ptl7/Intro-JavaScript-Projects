0bd6ae562a3ec4a83634d25450422a9e7936824e
uzz-ptl7 <uzair07patel@gmail.com>
2024-09-01 22:08:21 +0200

    Understood the key Concepts of Object Oriented Programming(OOP)

--- productMIS.js
+++ productMIS.js
@@ -16,10 +16,33 @@ class Product {
     }
 }
 
-let product1 = new Product('Laptop', 50000, 'Electronics');
+let product1 = new Product('T-Shirt', 1000, 'Clothing');
 let product2 = new Product('Shirt', 500, 'Clothing');
 
 product1.getInfo();
 product1.applyDiscount(10);
 product2.getInfo();
 product2.applyDiscount(20);
+
+class Electronics extends Product {
+    constructor(name, price, brand, warranty) {
+        super(name, price, 'Electronics');
+        this.brand = brand;
+        this.warranty = warranty;
+    }
+
+    getInfo() {
+        console.log(`\n\t Name: ${this.name} \n\t Price: ${this.price} \n\t Category: ${this.category}`);
+        console.log(`\t Brand: ${this.brand} \n\t Warranty: ${this.warranty}`);
+    }
+}
+
+let product3 = new Electronics('Laptop', 50000, 'Hp', '1 year');
+let product4 = new Electronics('Mobile', 20000, 'Samsung', '2 years');
+
+product3.getInfo();
+// product3.getElectronicsInfo();
+product3.applyDiscount(10);
+product4.getInfo();
+// product4.getElectronicsInfo();
+product4.applyDiscount(20);
